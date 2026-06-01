bbb6e682ed3ae59ae883b0a5ce31a1d0049cf984
tswe18600-lab <tswe18600@gmail.com>
2026-06-01 08:32:24 -0700

    Add files via upload

---
+++ config/mail_helper.php
@@ -0,0 +1,33 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Error တက်နေတဲ့ DIR နေရာမှာ ဒီလမ်းကြောင်းတွေနဲ့ အစားထိုးပါ
+require_once __DIR__ . '/../phpmailer/Exception.php';
+require_once __DIR__ . '/../phpmailer/PHPMailer.php';
+require_once __DIR__ . '/../phpmailer/SMTP.php';
+
+function sendStatusEmail($user_email, $user_name, $status, $service_name) {
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'tswe18600@gmail.com'; 
+        $mail->Password   = 'cubu exdd ilrj jxfy'; 
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        $mail->setFrom('tswe18600@gmail.com', 'Meiktila Service');
+        $mail->addAddress($user_email, $user_name);
+
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = "Booking Update - " . $service_name;
+        $mail->Body    = "မင်္ဂလာပါ $user_name,<br>သင်၏ $service_name ဘိုကင်မှာ <b>$status</b> ဖြစ်သွားပါပြီ။";
+
+        return $mail->send();
+    } catch (Exception $e) {
+        return false;
+    }
+}
